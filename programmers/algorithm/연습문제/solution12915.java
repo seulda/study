ff70732b30864bc8550be6559827cc201fdc8d8f
Daseul Kim <kim02977@naver.com>
2021-08-24 00:11:15 +0900

    문자열 내 마음대로 정렬하기
    문제 설명
    - 문자열로 구성된 리스트 strings와, 정수 n이 주어졌을 때, 각 문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬하려 합니다. 예를 들어 strings가 ["sun", "bed", "car"]이고 n이 1이면 각 단어의 인덱스 1의 문자 "u", "e", "a"로 strings를 정렬합니다.
    
    제한 조건
    - strings는 길이 1 이상, 50이하인 배열입니다.
    - strings의 원소는 소문자 알파벳으로 이루어져 있습니다.
    - strings의 원소는 길이 1 이상, 100이하인 문자열입니다.
    - 모든 strings의 원소의 길이는 n보다 큽니다.
    - 인덱스 1의 문자가 같은 문자열이 여럿 일 경우, 사전순으로 앞선 문자열이 앞쪽에 위치합니다.

--- programmers/algorithm/연습문제/solution12915.java
+++ programmers/algorithm/연습문제/solution12915.java
@@ -1,1 +1,22 @@
-
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        String[] arr = strings;
+        for(int i = 0; i < strings.length; i++) {
+            for(int j = 0; j < strings.length; j++) {
+                int test = String.valueOf(arr[i].charAt(n)).compareTo(String.valueOf(arr[j].charAt(n)));
+                if (test < 0) {
+                    String temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+                else if (test == 0 && arr[i].compareTo(arr[j]) < 0) {
+                    String temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        
+        return arr;
+    }
+}
