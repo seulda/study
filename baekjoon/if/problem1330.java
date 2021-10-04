d0e01eb1f8d58ea3ff3b3972138b751245a1f2ab
Daseul Kim <kim02977@naver.com>
2021-10-04 23:33:22 +0900

    두 수 비교하기

---
+++ baekjoon/if/problem1330.java
@@ -0,0 +1,21 @@
+// 두 수 비교하기
+
+// 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
+
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		if (a>b) {
+			System.out.println(">");
+		} else if (a<b) {
+			System.out.println("<");
+		} else {
+			System.out.println("==");
+		}
+	}
+}
